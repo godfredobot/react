f3ee64c577f075cb98fbb4392cadab0e728da8f0
uzochukwuV <vic.ezealor@gmail.com>
2023-01-23 19:24:35 -0800

    fixed dashboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,20 +1,7 @@
-import { Form } from "react-router-dom";
+
 
 function Dashboard() {
-    const contact = {
-        first: "Your",
-        last: "Name",
-        avatar: "https://placekitten.com/g/200/200",
-        twitter: "your_handle",
-        notes: "Some notes",
-        favorite: true,
-    };
-
-    return (
-        <div id="contact">
-            Dashboard
-        </div>
-    );
+    return <div id="contact"> Dashboard </div>
 }
 
 export default Dashboard;
