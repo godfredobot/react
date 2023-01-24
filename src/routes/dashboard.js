85c1571e2613526c5dae241482286ec4bec054df
uzochukwuV <vic.ezealor@gmail.com>
2023-01-23 19:21:58 -0800

    fixed dashboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -12,77 +12,9 @@ function Dashboard() {
 
     return (
         <div id="contact">
-            <div>
-                <img
-                    key={contact.avatar}
-                    src={contact.avatar || null}
-                    alt="profile pics"
-                />
-            </div>
-
-            <div>
-                <h1>
-                    {contact.first || contact.last ? (
-                        <>
-                            {contact.first} {contact.last}
-                        </>
-                    ) : (
-                        <i>No Name</i>
-                    )}{" "}
-                    <Favorite contact={contact} />
-                </h1>
-
-                {contact.twitter && (
-                    <p>
-                        <a
-                            target="_blank"
-                            href={`https://twitter.com/${contact.twitter}`}
-                            rel="noreferrer"
-                        >
-                            {contact.twitter}
-                        </a>
-                    </p>
-                )}
-
-                {contact.notes && <p>{contact.notes}</p>}
-
-                <div>
-                    <Form action="edit">
-                        <button type="submit">Edit</button>
-                    </Form>
-                    <Form
-                        method="post"
-                        action="destroy"
-                        onSubmit={(event) => {
-                            alert('delete');
-                        }}
-                    >
-                        <button type="submit">Delete</button>
-                    </Form>
-                </div>
-            </div>
+            Dashboard
         </div>
     );
 }
 
 export default Dashboard;
-
-function Favorite({ contact }) {
-    // yes, this is a `let` for later
-    let favorite = contact.favorite;
-    return (
-        <Form method="post">
-            <button
-                name="favorite"
-                value={favorite ? "false" : "true"}
-                aria-label={
-                    favorite
-                        ? "Remove from favorites"
-                        : "Add to favorites"
-                }
-            >
-                {favorite ? "★" : "☆"}
-            </button>
-        </Form>
-    );
-}
